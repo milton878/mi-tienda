555bf713c115953133767a89c82a8ed17b9a5f13
milton <miltoncaraball@yahoo.com.ar>
2022-07-05 19:41:56 -0300

    eventos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { ItemCount } from './components/ItemCount/ItemCount';
+
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import {NavBar} from './components/NavBar/NavBar';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
-  const onAdd = (count)=>{
-    alert(`Agregaste ${count} items al carrito`)
-  }
+  
   return (
     <>
     {
@@ -19,6 +17,10 @@ function App() {
         <Route path='/' element={<ItemListContainer />}/>
         <Route path='/category/:categoryId' element={<ItemListContainer />}/>
         <Route path='/item/:itemId' element={<ItemDetailContainer />}/>
+        
+
+       
+
 
 
 
@@ -30,8 +32,9 @@ function App() {
       </BrowserRouter>
       /*
       
+            <ItemCount  stock={5} initial={1} onAdd={onAdd} />
       
-      <ItemCount  stock={5} initial={1} onAdd={onAdd} />
+      
       
       */
     }
--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {ItemCount} from '../ItemCount/ItemCount'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export const ItemDetail = ({title, despcription, price, pictureUrl}) => {
+ const[goToCart, setGoCart] = useState(false);
+ 
+  const onAdd = (count)=>{
+    setGoCart(true);
+    // alert(`Agregaste ${count} items al carrito`)
+  }
   return (
     <div id='item-detail'>
         <div id='portada'>
@@ -10,6 +20,14 @@ export const ItemDetail = ({title, despcription, price, pictureUrl}) => {
             <h1>{title}</h1>
             <h2>{despcription}</h2>
             <h3>${price}</h3>
+            {
+              goToCart
+              ? <Link to='/cart'>Terminar Compra</Link>
+              :
+              <ItemCount  stock={5} initial={1} onAdd={onAdd} />
+            }
+            
+
         </div>
 
     </div>
